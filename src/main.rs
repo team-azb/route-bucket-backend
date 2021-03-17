8ceed140c4d335622eedc0f608a4a28bc6d17388
shugo256 <shugo256@gmail.com>
2021-03-17 20:41:09 +0900

    feat: use repository to register new route

--- src/main.rs
+++ src/main.rs
@@ -2,48 +2,50 @@
 extern crate diesel;
 
 mod domain;
-mod lib;
 mod infrastructure;
+mod lib;
 
-use actix_web::{HttpResponse, get, HttpServer, App, Error};
 use actix_web::middleware::Logger;
-use dotenv::dotenv;
-use diesel::prelude::*;
+use actix_web::{get, App, Error, HttpResponse, HttpServer};
+use bigdecimal::BigDecimal;
 use diesel::mysql::MysqlConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
 use nanoid::nanoid;
 
+use crate::domain::coordinate::Coordinate;
 use crate::domain::route::{Route, RouteRepository};
+use crate::domain::types::RouteId;
 use crate::infrastructure::dto::route::RouteDto;
 use crate::infrastructure::repository::route::RouteRepositoryMysql;
 use crate::infrastructure::schema;
-use crate::domain::types::RouteId;
 
 #[get("/")]
 async fn index() -> Result<HttpResponse, Error> {
     use schema::routes::dsl::*;
 
     let conn = establish_connection();
-    let route_id = nanoid!(11);
-    let new_route = RouteDto {
-        id: route_id.clone(),
-        name: "sample route".to_string()
-    };
-
-    diesel::insert_into(routes)
-        .values(new_route)
-        .execute(&conn)
-        .expect("failed inserting");
-
     let repository = RouteRepositoryMysql::new(conn);
-    let route = repository.find(RouteId(route_id))?;
+
+    let route = Route::new(
+        RouteId::new(),
+        "sample route".to_string(),
+        vec![
+            Coordinate::new(BigDecimal::from(35.0), BigDecimal::from(130.0))?,
+            Coordinate::new(BigDecimal::from(45.0), BigDecimal::from(140.0))?,
+        ],
+    );
+
+    repository.register(&route)?;
+
+    let route = repository.find(&route.id())?;
     Ok(HttpResponse::Ok().body(format!("{:#?}", route)))
 }
 
 fn establish_connection() -> MysqlConnection {
     dotenv().ok();
 
-    let database_url = std::env::var("DATABASE_URL")
-        .expect("DATABASE_URL NOT FOUND");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL NOT FOUND");
 
     MysqlConnection::establish(&database_url).expect("Error on db connection!")
 }
@@ -53,9 +55,13 @@ async fn main() -> Result<(), Error> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
-    HttpServer::new(move || App::new().wrap(Logger::new("%a \"%r\" %s (%T s)")).service(index))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await?;
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::new("%a \"%r\" %s (%T s)"))
+            .service(index)
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await?;
     Ok(())
 }
