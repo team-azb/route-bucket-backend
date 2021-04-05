1b2bdebf89eb8291708de54888f52f9d31c43143
shugo256 <shugo256@gmail.com>
2021-04-06 02:11:20 +0900

    lib: add num and polyline

---
+++ migrations/2021-04-05-091826_create_operations/down.sql
@@ -0,0 +1,11 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE operations;
+
+ALTER TABLE routes DROP `polyline`, `operation_pos`;
+CREATE TABLE coordinates (
+     PRIMARY KEY (`route_id`, `index`),
+     `route_id` VARCHAR(11) NOT NULL,
+     `index` INTEGER UNSIGNED NOT NULL,
+     `latitude` DECIMAL NOT NULL,
+     `longitude` DECIMAL NOT NULL
+);
