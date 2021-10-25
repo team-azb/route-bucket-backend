58f11dc7229a5d8b63557ae3dffdf5e67db09976
shugo256 <shugo256@gmail.com>
2021-10-25 15:16:19 +0000

    refactor: move fixtures into route::fixtures

--- api/domain/src/model.rs
+++ api/domain/src/model.rs
@@ -3,11 +3,13 @@ pub mod types;
 
 #[cfg(feature = "fixtures")]
 pub mod fixtures {
-    pub use super::route::bounding_box::tests::BoundingBoxFixture;
-    pub use super::route::coordinate::tests::CoordinateFixtures;
-    pub use super::route::operation::tests::OperationFixtures;
-    pub use super::route::route_gpx::tests::RouteGpxFixtures;
-    pub use super::route::route_info::tests::RouteInfoFixtures;
-    pub use super::route::segment_list::tests::{SegmentFixtures, SegmentListFixture};
-    pub use super::route::tests::RouteFixtures;
+    pub mod route {
+        pub use crate::model::route::bounding_box::tests::BoundingBoxFixture;
+        pub use crate::model::route::coordinate::tests::CoordinateFixtures;
+        pub use crate::model::route::operation::tests::OperationFixtures;
+        pub use crate::model::route::route_gpx::tests::RouteGpxFixtures;
+        pub use crate::model::route::route_info::tests::RouteInfoFixtures;
+        pub use crate::model::route::segment_list::tests::{SegmentFixtures, SegmentListFixture};
+        pub use crate::model::route::tests::RouteFixtures;
+    }
 }
