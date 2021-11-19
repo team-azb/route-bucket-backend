44e3795b1dfca5012713b43644cdca1aacfe69b9
shugo256 <shugo256@gmail.com>
2021-11-19 13:02:25 +0000

    refactor: replace UserAuthInfo with BearerAuth

--- api/controller/src/user.rs
+++ api/controller/src/user.rs
@@ -1,10 +1,7 @@
-use std::{future::Future, pin::Pin};
-
-use actix_web::{dev::Payload, web, FromRequest, HttpRequest, HttpResponse, Result};
-use derive_more::Constructor;
+use actix_web::{web, HttpResponse, Result};
+use actix_web_httpauth::extractors::bearer::BearerAuth;
 use route_bucket_domain::model::user::UserId;
 use route_bucket_usecase::user::{UserCreateRequest, UserUpdateRequest, UserUseCase};
-use route_bucket_utils::ApplicationError;
 
 use crate::AddService;
 
@@ -24,58 +21,23 @@ async fn post<U: 'static + UserUseCase>(
 
 async fn patch<U: 'static + UserUseCase>(
     usecase: web::Data<U>,
-    auth_info: UserAuthInfo,
+    user_id: web::Path<UserId>,
+    auth: BearerAuth,
     req: web::Json<UserUpdateRequest>,
 ) -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().json(
         usecase
-            .update(&auth_info.id, &auth_info.token, req.into_inner())
+            .update(&user_id, auth.token(), req.into_inner())
             .await?,
     ))
 }
 
 async fn delete<U: 'static + UserUseCase>(
     usecase: web::Data<U>,
-    auth_info: UserAuthInfo,
+    user_id: web::Path<UserId>,
+    auth: BearerAuth,
 ) -> Result<HttpResponse> {
-    Ok(HttpResponse::Ok().json(usecase.delete(&auth_info.id, &auth_info.token).await?))
-}
-
-#[derive(Clone, Debug, Constructor)]
-struct UserAuthInfo {
-    id: UserId,
-    token: String,
-}
-
-impl FromRequest for UserAuthInfo {
-    type Config = ();
-    type Error = ApplicationError;
-    type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
-
-    fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
-        if let Some(auth_val) = req.headers().get("Authorization") {
-            if let Ok(auth_str) = auth_val.to_str() {
-                if auth_str.starts_with("bearer") || auth_str.starts_with("Bearer") {
-                    let id = req.match_info().load().unwrap();
-                    let token = auth_str[6..auth_str.len()].trim().to_string();
-                    return Box::pin(async move { Ok(UserAuthInfo::new(id, token)) });
-                }
-            }
-        }
-
-        Box::pin(async move { Err(ApplicationError::AuthError("Token not found!".into())) })
-    }
-
-    fn extract(req: &HttpRequest) -> Self::Future {
-        Self::from_request(req, &mut actix_web::dev::Payload::None)
-    }
-
-    fn configure<F>(f: F) -> Self::Config
-    where
-        F: FnOnce(Self::Config) -> Self::Config,
-    {
-        f(())
-    }
+    Ok(HttpResponse::Ok().json(usecase.delete(&user_id, auth.token()).await?))
 }
 
 pub trait BuildUserService: AddService {
