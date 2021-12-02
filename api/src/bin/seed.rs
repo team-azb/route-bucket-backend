847b814f53c5a9dde0f234577aba257b63f93af2
shugo256 <shugo256@gmail.com>
2021-12-02 11:12:02 +0000

    fix: comment out seed.rs

--- api/src/bin/seed.rs
+++ api/src/bin/seed.rs
@@ -1,80 +1,81 @@
-use route_bucket_backend::server::Server;
-use route_bucket_domain::model::route::{Coordinate, DrawingMode};
-use route_bucket_usecase::route::RouteUseCase;
+// use route_bucket_backend::server::Server;
+// use route_bucket_domain::model::route::{Coordinate, DrawingMode};
+// use route_bucket_usecase::route::RouteUseCase;
 
-macro_rules! coord {
-    ( $lat: expr, $lon: expr ) => {
-        Coordinate::new($lat, $lon).unwrap()
-    };
-}
+// macro_rules! coord {
+//     ( $lat: expr, $lon: expr ) => {
+//         Coordinate::new($lat, $lon).unwrap()
+//     };
+// }
 
 #[actix_web::main]
 async fn main() {
-    env_logger::init();
+    todo!()
+    // env_logger::init();
 
-    let server = Server::new().await;
+    // let server = Server::new().await;
 
-    let route_id1 = server
-        .create(&String::from("sample1").into())
-        .await
-        .unwrap()
-        .id;
+    // let route_id1 = server
+    //     .create(&String::from("sample1").into())
+    //     .await
+    //     .unwrap()
+    //     .id;
 
-    let route_id2 = server
-        .create(&String::from("sample2: 皇居ラン").into())
-        .await
-        .unwrap()
-        .id;
+    // let route_id2 = server
+    //     .create(&String::from("sample2: 皇居ラン").into())
+    //     .await
+    //     .unwrap()
+    //     .id;
 
-    server
-        .add_point(
-            &route_id2,
-            0,
-            &(DrawingMode::FollowRoad, coord!(35.68136, 139.75875)).into(),
-        )
-        .await
-        .unwrap();
-    server
-        .add_point(
-            &route_id2,
-            1,
-            &(DrawingMode::FollowRoad, coord!(35.69053, 139.75681)).into(),
-        )
-        .await
-        .unwrap();
-    server
-        .add_point(
-            &route_id2,
-            2,
-            &(DrawingMode::FollowRoad, coord!(35.69510, 139.75139)).into(),
-        )
-        .await
-        .unwrap();
-    server
-        .add_point(
-            &route_id2,
-            3,
-            &(DrawingMode::FollowRoad, coord!(35.68942, 139.74547)).into(),
-        )
-        .await
-        .unwrap();
-    server
-        .add_point(
-            &route_id2,
-            4,
-            &(DrawingMode::FollowRoad, coord!(35.68418, 139.74424)).into(),
-        )
-        .await
-        .unwrap();
-    server
-        .add_point(
-            &route_id2,
-            5,
-            &(DrawingMode::FollowRoad, coord!(35.68136, 139.75875)).into(),
-        )
-        .await
-        .unwrap();
+    // server
+    //     .add_point(
+    //         &route_id2,
+    //         0,
+    //         &(DrawingMode::FollowRoad, coord!(35.68136, 139.75875)).into(),
+    //     )
+    //     .await
+    //     .unwrap();
+    // server
+    //     .add_point(
+    //         &route_id2,
+    //         1,
+    //         &(DrawingMode::FollowRoad, coord!(35.69053, 139.75681)).into(),
+    //     )
+    //     .await
+    //     .unwrap();
+    // server
+    //     .add_point(
+    //         &route_id2,
+    //         2,
+    //         &(DrawingMode::FollowRoad, coord!(35.69510, 139.75139)).into(),
+    //     )
+    //     .await
+    //     .unwrap();
+    // server
+    //     .add_point(
+    //         &route_id2,
+    //         3,
+    //         &(DrawingMode::FollowRoad, coord!(35.68942, 139.74547)).into(),
+    //     )
+    //     .await
+    //     .unwrap();
+    // server
+    //     .add_point(
+    //         &route_id2,
+    //         4,
+    //         &(DrawingMode::FollowRoad, coord!(35.68418, 139.74424)).into(),
+    //     )
+    //     .await
+    //     .unwrap();
+    // server
+    //     .add_point(
+    //         &route_id2,
+    //         5,
+    //         &(DrawingMode::FollowRoad, coord!(35.68136, 139.75875)).into(),
+    //     )
+    //     .await
+    //     .unwrap();
 
-    log::info!("Route {} added!", route_id1);
-    log::info!("Route {} added!", route_id2);
+    // log::info!("Route {} added!", route_id1);
+    // log::info!("Route {} added!", route_id2);
 }
