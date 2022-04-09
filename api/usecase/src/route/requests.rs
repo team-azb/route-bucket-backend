cb6b76a13b4f2f81ba22e73c75c0464ce4a2298c
shugo256 <shugo256@gmail.com>
2022-04-09 08:04:30 +0000

    feat: Add validation for routes endpoint requests.

--- api/usecase/src/route/requests.rs
+++ api/usecase/src/route/requests.rs
@@ -1,5 +1,6 @@
 use derive_more::From;
 use serde::Deserialize;
+use validator::{Validate, ValidationError};
 
 use route_bucket_domain::model::{
     permission::PermissionType,
@@ -7,36 +8,53 @@ use route_bucket_domain::model::{
     user::UserId,
 };
 
-#[derive(From, Deserialize)]
+#[derive(From, Deserialize, Validate)]
 pub struct RouteCreateRequest {
+    #[validate(length(min = 1, max = 50))]
     pub(super) name: String,
     pub(super) is_public: bool,
 }
 
-#[derive(From, Deserialize)]
+#[derive(From, Deserialize, Validate)]
 pub struct NewPointRequest {
     pub(super) mode: DrawingMode,
+    // TODO: Validate coordinate range.
     pub(super) coord: Coordinate,
 }
 
-#[derive(From, Deserialize)]
+#[derive(From, Deserialize, Validate)]
 pub struct RemovePointRequest {
     pub(super) mode: DrawingMode,
 }
 
-#[derive(From, Deserialize)]
+#[derive(From, Deserialize, Validate)]
 pub struct RouteRenameRequest {
+    #[validate(length(min = 1, max = 50))]
     pub(super) name: String,
 }
 
-#[derive(From, Deserialize)]
+#[derive(From, Deserialize, Validate)]
 pub struct UpdatePermissionRequest {
+    #[validate]
     pub(super) user_id: UserId,
     #[serde(alias = "type")]
+    #[validate(custom = "Self::validate_permission_type")]
     pub(super) permission_type: PermissionType,
 }
 
-#[derive(From, Deserialize)]
+impl UpdatePermissionRequest {
+    // NOTE: validatorのrangeはnumber以外にはなぜか対応していない
+    // 参考： https://github.com/Keats/validator/issues/204
+    fn validate_permission_type(permission_type: &PermissionType) -> Result<(), ValidationError> {
+        match *permission_type {
+            PermissionType::Viewer | PermissionType::Editor => Ok(()),
+            _ => Err(ValidationError::new("Only VIEWER and EDITOR are allowed.")),
+        }
+    }
+}
+
+#[derive(From, Deserialize, Validate)]
 pub struct DeletePermissionRequest {
+    #[validate]
     pub(super) user_id: UserId,
 }
