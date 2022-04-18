c82a1b3b4aa8ba90e1e5612332a59b6b3d573283
shugo256 <shugo256@gmail.com>
2022-04-18 05:29:39 +0000

    refactor: Define FloatValueObject as a type alias.

--- api/domain/src/model/route/types.rs
+++ api/domain/src/model/route/types.rs
@@ -1,7 +1,7 @@
 use std::convert::TryFrom;
 
 use derive_more::{Add, AddAssign, Display, From, Into, Sub, Sum};
-use num_traits::{Bounded, Float, FromPrimitive, Signed, Zero};
+use num_traits::{Bounded, Float, FromPrimitive, Zero};
 use ordered_float::OrderedFloat;
 use serde::{Deserialize, Serialize};
 
@@ -24,7 +24,7 @@ pub type Longitude = FloatValueObject<f64, 180, -180>;
 pub type Elevation = NumericValueObject<i32>;
 pub type Distance = FloatValueObject<f64, { i64::MAX }>;
 
-/// Value Object for BigDecimal type
+/// Value Object for Numeric type
 #[derive(
     Default,
     Add,
@@ -43,9 +43,20 @@ pub type Distance = FloatValueObject<f64, { i64::MAX }>;
 )]
 pub struct NumericValueObject<T, const MAX: i64 = 0, const MIN: i64 = 0>(T);
 
-impl<T: Copy, const MAX: i64, const MIN: i64> NumericValueObject<T, MAX, MIN> {
-    pub fn value(&self) -> T {
+type FloatValueObject<T, const MAX: i64 = 0, const MIN: i64 = 0> =
+    NumericValueObject<OrderedFloat<T>, MAX, MIN>;
+
+// NOTE: Tに対して実装しようとすると、下のFloatへの実装とconflictする
+// traitの特殊化待ち: https://github.com/rust-lang/rust/issues/31844
+impl<const MAX: i64, const MIN: i64> NumericValueObject<i32, MAX, MIN> {
+    pub fn value(&self) -> i32 {
         self.0
+    }
+}
+
+impl<T: Float, const MAX: i64, const MIN: i64> FloatValueObject<T, MAX, MIN> {
+    pub fn value(&self) -> T {
+        self.0.into_inner()
     }
 }
 
@@ -59,31 +70,37 @@ impl<T: Zero, const MAX: i64, const MIN: i64> Zero for NumericValueObject<T, MAX
     }
 }
 
-impl<T: Bounded + Signed + FromPrimitive, const MAX: i64, const MIN: i64> Bounded
-    for NumericValueObject<T, MAX, MIN>
-{
-    fn min_value() -> Self {
-        Self(if MAX > MIN {
-            T::from_i64(MIN).unwrap()
-        } else {
-            T::min_value()
-        })
-    }
-
-    fn max_value() -> Self {
-        Self(if MAX > MIN {
-            T::from_i64(MAX).unwrap()
-        } else {
-            T::max_value()
-        })
+impl<T, const MAX: i64, const MIN: i64> NumericValueObject<T, MAX, MIN> {
+    fn has_limit() -> bool {
+        MIN < MAX
     }
 }
 
-pub trait ValidatableInner: PartialOrd + Bounded + Signed + FromPrimitive {}
-impl<T: PartialOrd + Bounded + Signed + FromPrimitive> ValidatableInner for T {}
-
-impl<T: ValidatableInner, const MAX: i64, const MIN: i64> Validate
+impl<T: Bounded + FromPrimitive, const MAX: i64, const MIN: i64> Bounded
     for NumericValueObject<T, MAX, MIN>
+{
+    fn min_value() -> Self {
+        Self(
+            Self::has_limit()
+                .then(|| MIN)
+                .and_then(T::from_i64)
+                .unwrap_or_else(T::min_value),
+        )
+    }
+
+    fn max_value() -> Self {
+        Self(
+            Self::has_limit()
+                .then(|| MAX)
+                .and_then(T::from_i64)
+                .unwrap_or_else(T::max_value),
+        )
+    }
+}
+
+impl<T, const MAX: i64, const MIN: i64> Validate for NumericValueObject<T, MAX, MIN>
+where
+    Self: Bounded + PartialOrd,
 {
     fn validate(&self) -> Result<(), ValidationErrors> {
         (Self::min_value()..=Self::max_value())
@@ -109,84 +126,13 @@ impl<const MAX: i64, const MIN: i64> TryFrom<i32> for NumericValueObject<i32, MA
     }
 }
 
-pub trait InnerFloat: ValidatableInner + Float {}
-impl<T: ValidatableInner + Float> InnerFloat for T {}
-
-#[derive(
-    Default,
-    Add,
-    AddAssign,
-    Sub,
-    Sum,
-    Copy,
-    Clone,
-    Debug,
-    PartialEq,
-    PartialOrd,
-    Serialize,
-    Deserialize,
-    Validate,
-)]
-pub struct FloatValueObject<T: InnerFloat, const MAX: i64 = 0, const MIN: i64 = 0> {
-    #[validate]
-    value: NumericValueObject<OrderedFloat<T>, MAX, MIN>,
-}
-
-impl<T: InnerFloat, const MAX: i64, const MIN: i64> FloatValueObject<T, MAX, MIN> {
-    pub fn value(&self) -> T {
-        self.value.value().into_inner()
-    }
-}
-
-impl<T: InnerFloat + Zero, const MAX: i64, const MIN: i64> Zero for FloatValueObject<T, MAX, MIN> {
-    fn zero() -> Self {
-        Self {
-            value: NumericValueObject::<OrderedFloat<T>, MAX, MIN>::zero(),
-        }
-    }
-
-    fn is_zero(&self) -> bool {
-        self.value.is_zero()
-    }
-}
-
-impl<T: InnerFloat, const MAX: i64, const MIN: i64> Bounded for FloatValueObject<T, MAX, MIN> {
-    fn min_value() -> Self {
-        Self {
-            value: NumericValueObject::<OrderedFloat<T>, MAX, MIN>::min_value(),
-        }
-    }
-
-    fn max_value() -> Self {
-        Self {
-            value: NumericValueObject::<OrderedFloat<T>, MAX, MIN>::max_value(),
-        }
-    }
-}
-
 // NOTE: TryFrom<T>を実装しようとすると、coreのimplとconflictする
 impl<const MAX: i64, const MIN: i64> TryFrom<f64> for FloatValueObject<f64, MAX, MIN> {
     type Error = ApplicationError;
 
     fn try_from(val: f64) -> ApplicationResult<Self> {
-        let res = Self {
-            value: NumericValueObject::<_, MAX, MIN>(OrderedFloat(val)),
-        };
+        let res = Self(OrderedFloat(val));
         res.validate()?;
         Ok(res)
-    }
-}
-
-// NOTE: derive(Eq, Ord)ではダメで、なぜか手で実装する必要があった
-impl<T: InnerFloat, const MAX: i64, const MIN: i64> Eq for FloatValueObject<T, MAX, MIN> where
-    FloatValueObject<T, MAX, MIN>: PartialEq
-{
-}
-impl<T: InnerFloat, const MAX: i64, const MIN: i64> Ord for FloatValueObject<T, MAX, MIN>
-where
-    FloatValueObject<T, MAX, MIN>: Eq,
-{
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.value.cmp(&other.value)
     }
 }
