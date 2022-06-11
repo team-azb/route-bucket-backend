fdf59453d0deb6f6b422a0dfef16012c2f64ebc5
shugo256 <shugo256@gmail.com>
2022-06-11 07:48:49 +0000

    feat: import usecase from client.

--- app/client/src/main.rs
+++ app/client/src/main.rs
@@ -1,10 +1,37 @@
-use route_bucket_domain::model::route::RouteInfo;
+use std::str::FromStr;
+
+use chrono::NaiveDate;
+use route_bucket_domain::model::{
+    route::RouteInfo,
+    types::{Email, Url},
+    user::{Gender, UserId},
+};
+use route_bucket_usecase::user::UserCreateRequest;
 use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
+    let req = UserCreateRequest::from((
+        UserId::from("hoge".to_string()),
+        "hoge".to_string(),
+        Email::try_from("sample@email.com".to_string()).unwrap(),
+        Gender::Male,
+        NaiveDate::from_str("2000-01-01").ok(),
+        Url::try_from("https://google.com".to_string()).ok(),
+        "password".to_string(),
+    ));
+    let (user, email, _) = req.into();
     html! {
-        <h1>{ format!("hello {:?}!", RouteInfo::default()) }</h1>
+        <>
+            <h1>{ format!("Hello {} ({:?})!", user.name(), email) }</h1>
+            <div style="white-space: pre-wrap;">
+                { format!("User: {:#?}!", user) }
+            </div>
+            <br/>
+            <div style="white-space: pre-wrap;">
+                { format!("Route: {:#?}", RouteInfo::default()) }
+            </div>
+        </>
     }
 }
 
